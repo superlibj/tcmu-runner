a233ba35d559b0110dae46de330f54d82ba298d6
Xiubo Li <lixiubo@cmss.chinamobile.com>
2017-01-13 18:01:53 +0800

    thread
    Signed-off-by: Xiubo Li <lixiubo@cmss.chinamobile.com>
    Signed-off-by: Jianfei Hu <hujianfei@cmss.chinamobile.com>

--- libtcmu_log.c
+++ libtcmu_log.c
@@ -15,12 +15,76 @@
  */
 
 #define _GNU_SOURCE
+#include <stdint.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <pthread.h>
+
+#include "darray.h"
 #include "libtcmu_log.h"
 #include "libtcmu_config.h"
 
-static int tcmu_log_level = TCMU_LOG_WARN;
+pthread_mutex_t g_mutex;  
 
-static inline int tcmu_log_level_conf_to_syslog(int level)
+/* Atomic Operations */
+typedef struct {
+       volatile int counter;
+} atomic_t;
+
+#define ATOMIC_INIT(i) {(i)}
+atomic_t g_mutex_initializer = ATOMIC_INIT(0);
+static inline int atomic_compare_and_set(atomic_t *v, int oldval, int newval)
+{
+	return __sync_bool_compare_and_swap(&v->counter, oldval, newval);
+}
+
+/* tcmu ring buffer for log */
+#define LOG_ENTRY_LEN 256 /* rb[0] is reserved for pri */
+#define LOG_MSG_LEN (LOG_ENTRY_LEN - 1) /* the length of the log message */
+#define LOG_ENTRYS (1024 * 32)
+
+struct log_buf {
+	pthread_cond_t cond;
+	pthread_mutex_t lock;
+
+	bool thread_active;
+	bool finish_initialize;
+
+	unsigned int head;
+	unsigned int tail;
+	char buf[LOG_ENTRYS][LOG_ENTRY_LEN];
+};
+
+struct log_rb {
+	pthread_t thread_id;
+	pid_t pid;
+	bool rb_closed;
+	struct log_buf *logbuf;
+};
+
+static darray(struct log_rb) log_rbs = darray_new();
+
+static int tcmu_log_level = TCMU_LOG_WARN;
+static bool tcmu_log_initialize(void);
+
+static inline struct log_rb *current_rb(void)
+{
+	struct log_rb *r;
+
+	darray_foreach(r, log_rbs) {
+		if (r->pid == getpid())
+			return r;
+	}
+
+	return NULL;
+}
+
+/* covert log level from tcmu config to syslog */
+static inline int to_syslog_level(int level)
 {
 	switch (level) {
 		case TCMU_CONF_LOG_ERROR:
@@ -30,6 +94,7 @@ static inline int tcmu_log_level_conf_to_syslog(int level)
 		case TCMU_CONF_LOG_INFO:
 			return TCMU_LOG_INFO;
 		case TCMU_CONF_LOG_DEBUG:
+
 			return TCMU_LOG_DEBUG;
 		default:
 			return TCMU_LOG_WARN;
@@ -42,40 +107,95 @@ unsigned int tcmu_get_log_level(void)
 	return tcmu_log_level;
 }
 
-/* covert log level from tcmu config to syslog */
 void tcmu_set_log_level(int level)
 {
-	tcmu_log_level = tcmu_log_level_conf_to_syslog(level);
+	tcmu_log_level = to_syslog_level(level);
 }
 
-void tcmu_log_open_syslog(const char *ident, int option, int facility)
+static void open_syslog(const char *ident, int option, int facility)
 {
-	const char *id = TCMU_IDENT;
+#define ID_MAX_LEN 16
+	char id[ID_MAX_LEN] = {0}, path[128];
+	int fd, len;
 
-	if (ident)
-		id = ident;
+	if (!ident) {
+		sprintf(path, "/proc/%d/comm", getpid());
+		fd = open(path, O_RDONLY);
+			if (fd < 0)
+				return;
+		len = read(fd, id, ID_MAX_LEN);
+		if (len < 0) {
+			close(fd);
+			return;
+		}
+		close(fd);
+	} else {
+		strncpy(id, ident, ID_MAX_LEN);
+	}
 
 	openlog(id, option, facility);
 }
 
-void tcmu_log_close_syslog(void)
+static void close_syslog(void)
 {
 	closelog();
 }
 
-static inline void tcmu_log_to_syslog(int pri, const char *logbuf)
+static inline void log_to_syslog(int pri, const char *logbuf)
 {
 	syslog(pri, "%s", logbuf);
 }
 
-static void
-tcmu_log_internal(int pri,
-		  const char *funcname,
-		  int linenr,
-		  const char *fmt,
-		  va_list args)
+static inline uint8_t rb_get_pri(struct log_buf *logbuf, unsigned int cur)
 {
-	char logbuf[TCMU_LOG_BUF_SIZE];
+	return logbuf->buf[cur][0];
+}
+
+static inline void rb_set_pri(struct log_buf *logbuf, unsigned int cur, uint8_t pri)
+{
+	logbuf->buf[cur][0] = (char)pri;
+}
+
+static inline char *rb_get_msg(struct log_buf *logbuf, unsigned int cur)
+{
+	return logbuf->buf[cur] + 1;
+}
+
+static inline bool rb_is_empty(struct log_buf *logbuf)
+{
+	return logbuf->tail == logbuf->head;
+}
+
+static inline bool rb_is_full(struct log_buf *logbuf)
+{
+	return logbuf->tail == (logbuf->head + 1) % LOG_ENTRYS;
+}
+
+static inline void rb_update_tail(struct log_buf *logbuf)
+{
+	logbuf->tail = (logbuf->tail + 1) % LOG_ENTRYS;
+}
+
+static inline void rb_update_head(struct log_buf *logbuf)
+{
+	/* when the ring buffer is full, the oldest log will be dropped */
+	if (rb_is_full(logbuf))
+		rb_update_tail(logbuf);
+
+	logbuf->head = (logbuf->head + 1) % LOG_ENTRYS;
+}
+
+static void
+log_internal(int pri,
+	     const char *funcname,
+	     int linenr,
+	     const char *fmt,
+	     va_list args)
+{
+	struct log_rb *r;
+	struct log_buf *logbuf;
+	unsigned int head;
+	char *msg;
 	int n;
 
 	if (pri > tcmu_log_level)
@@ -84,10 +204,33 @@ tcmu_log_internal(int pri,
 	if (!fmt)
 		return;
 
-	n = sprintf(logbuf, "%s:%d : ", funcname, linenr);
-	vsnprintf(logbuf + n, TCMU_LOG_BUF_SIZE - n - 1, fmt, args);
+	if (!tcmu_log_initialize())
+		return;
 
-	tcmu_log_to_syslog(pri, logbuf);
+	pthread_mutex_lock(&g_mutex);
+	r = current_rb();
+	if (!r || r->rb_closed) {
+		pthread_mutex_unlock(&g_mutex);
+		return;
+	}
+
+	logbuf = r->logbuf;
+
+	pthread_mutex_lock(&logbuf->lock);
+
+	head = logbuf->head;
+	rb_set_pri(logbuf, head, pri);
+	msg = rb_get_msg(logbuf, head);
+	n = sprintf(msg, "%s:%d : ", funcname, linenr);
+	vsnprintf(msg + n, LOG_MSG_LEN - n, fmt, args);
+
+	rb_update_head(logbuf);
+
+	if (logbuf->thread_active == false)
+		pthread_cond_signal(&logbuf->cond);
+
+	pthread_mutex_unlock(&logbuf->lock);
+	pthread_mutex_unlock(&g_mutex);
 }
 
 void tcmu_err_message(const char *funcname, int linenr, const char *fmt, ...)
@@ -95,7 +238,7 @@ void tcmu_err_message(const char *funcname, int linenr, const char *fmt, ...)
 	va_list args;
 
 	va_start(args, fmt);
-	tcmu_log_internal(TCMU_LOG_ERROR, funcname, linenr, fmt, args);
+	log_internal(TCMU_LOG_ERROR, funcname, linenr, fmt, args);
 	va_end(args);
 }
 
@@ -104,7 +247,7 @@ void tcmu_warn_message(const char *funcname, int linenr, const char *fmt, ...)
 	va_list args;
 
 	va_start(args, fmt);
-	tcmu_log_internal(TCMU_LOG_WARN, funcname, linenr, fmt, args);
+	log_internal(TCMU_LOG_WARN, funcname, linenr, fmt, args);
 	va_end(args);
 }
 
@@ -113,14 +256,222 @@ void tcmu_info_message(const char *funcname, int linenr, const char *fmt, ...)
 	va_list args;
 
 	va_start(args, fmt);
-	tcmu_log_internal(TCMU_LOG_INFO, funcname, linenr, fmt, args);
+	log_internal(TCMU_LOG_INFO, funcname, linenr, fmt, args);
 	va_end(args);
 }
+
 void tcmu_dbg_message(const char *funcname, int linenr, const char *fmt, ...)
 {
 	va_list args;
 
 	va_start(args, fmt);
-	tcmu_log_internal(TCMU_LOG_DEBUG, funcname, linenr, fmt, args);
+	log_internal(TCMU_LOG_DEBUG, funcname, linenr, fmt, args);
 	va_end(args);
+}
+
+static void log_output(int pri, const char *msg)
+{
+	log_to_syslog(pri, msg);
+}
+
+static bool log_buf_not_empty_output(void)
+{	
+	struct log_rb *r;
+	struct log_buf *logbuf;
+	unsigned int tail;
+	uint8_t pri;
+	char *msg, buf[LOG_MSG_LEN];
+	
+	pthread_mutex_lock(&g_mutex);
+	r = current_rb();
+	if (!r) {
+		pthread_mutex_unlock(&g_mutex);
+		return false;
+	}
+	logbuf = r->logbuf;
+
+	pthread_mutex_lock(&logbuf->lock);
+	if (rb_is_empty(logbuf)) {
+		pthread_mutex_unlock(&logbuf->lock);
+		pthread_mutex_unlock(&g_mutex);
+		return false;
+	}
+
+	tail = logbuf->tail;
+	pri = rb_get_pri(logbuf, tail);
+	msg = rb_get_msg(logbuf, tail);
+	memcpy(buf, msg, LOG_MSG_LEN);
+	rb_update_tail(logbuf);
+	pthread_mutex_unlock(&logbuf->lock);
+	pthread_mutex_unlock(&g_mutex);
+
+	/*
+ 	 * This may block due to rsyslog and syslog-ng, etc.
+ 	 * And the log productors could still insert their log
+ 	 * messages into the ring buffer without blocking. But
+ 	 * the ring buffer may lose some old log rbs if the
+ 	 * ring buffer is full.
+ 	 */
+	log_output(pri, buf);
+
+	return true;
+}
+
+static void log_rb_remove(void)
+{
+	struct log_rb *r;
+	unsigned int i = 0;
+
+	darray_foreach(r, log_rbs) {
+		if (r->pid != getpid())
+			i++;
+		else
+			darray_remove(log_rbs, i);
+	}
+}
+
+void tcmu_cancel_log_thread(void)
+{
+	pthread_t thread_id;
+	struct log_rb *r;
+	int ret;
+
+	pthread_mutex_lock(&g_mutex);
+	darray_foreach(r, log_rbs) {
+		if (r->pid == getpid()) {
+			thread_id = r->thread_id;
+			r->rb_closed = true;
+			break;
+		}
+	}
+	pthread_mutex_unlock(&g_mutex);
+
+	ret = pthread_cancel(thread_id);
+	if (ret)
+		return;
+
+	ret = pthread_join(thread_id, NULL);
+	if (ret)
+		return;
+}
+
+static void log_thread_cleanup(void *arg)
+{
+	struct log_buf *logbuf = arg;
+
+	pthread_mutex_lock(&g_mutex);
+	pthread_cond_destroy(&logbuf->cond);
+	pthread_mutex_destroy(&logbuf->lock);
+	pthread_mutex_destroy(&logbuf->lock);
+	log_rb_remove();
+	free(logbuf);
+	pthread_mutex_unlock(&g_mutex);
+
+	close_syslog();
+}
+
+
+static void *log_thread_start(void *arg)
+{
+	struct log_buf *logbuf = arg;
+
+	pthread_cleanup_push(log_thread_cleanup, arg);
+
+	open_syslog(NULL, 0, 0);
+
+	while (1) {
+		pthread_mutex_lock(&logbuf->lock);
+		if (!logbuf->finish_initialize) {
+			logbuf->finish_initialize = true;
+			pthread_cond_signal(&logbuf->cond);
+		}
+
+		logbuf->thread_active = false;
+		pthread_cond_wait(&logbuf->cond, &logbuf->lock);
+		logbuf->thread_active = true;
+		pthread_mutex_unlock(&logbuf->lock);
+
+		while (log_buf_not_empty_output());
+	}
+
+	pthread_cleanup_pop(1);
+	return NULL;
+}
+
+static inline bool g_mutex_has_initialized(void)
+{
+	return !atomic_compare_and_set(&g_mutex_initializer, 0, 1);
+}
+
+static void g_mutex_init(void)
+{
+	int ret;
+
+	if (g_mutex_has_initialized())
+		return;
+
+	pthread_mutexattr_t attr;
+	pthread_mutexattr_init(&attr);
+	ret = pthread_mutexattr_setpshared(&attr,PTHREAD_PROCESS_SHARED);
+	if( ret != 0 )
+	{
+		printf("pthread_mutexattr_setpshared error\n");
+		exit(1);
+	}
+	pthread_mutex_init(&g_mutex, &attr);
+}
+
+static bool tcmu_log_initialize(void)
+{
+	struct log_rb rb, *r;
+	int ret;
+
+	g_mutex_init();
+
+	pthread_mutex_lock(&g_mutex);
+	r = current_rb();
+	if (r) {
+		pthread_mutex_unlock(&g_mutex);
+		return true;
+	}
+
+	rb.logbuf = malloc(sizeof(struct log_buf));
+	if (!rb.logbuf) {
+		pthread_mutex_unlock(&g_mutex);
+		return false;
+	}
+
+	rb.pid = getpid();
+	rb.rb_closed = false;
+	rb.logbuf->thread_active = false;
+	rb.logbuf->finish_initialize = false;
+	rb.logbuf->head = 0;
+	rb.logbuf->tail = 0;
+	pthread_cond_init(&rb.logbuf->cond, NULL);
+	pthread_mutex_init(&rb.logbuf->lock, NULL);
+
+	ret = pthread_create(&rb.thread_id, NULL, log_thread_start, rb.logbuf);
+	if (ret) {
+		log_rb_remove();
+		free(rb.logbuf);
+		pthread_mutex_unlock(&g_mutex);
+		return false;
+	}
+
+	darray_append(log_rbs, rb);
+	r = current_rb();
+	if (!r) {
+		log_rb_remove();
+		free(rb.logbuf);
+		pthread_mutex_unlock(&g_mutex);
+		return false;
+	}
+
+	pthread_mutex_lock(&r->logbuf->lock);
+	while (!r->logbuf->finish_initialize)
+		pthread_cond_wait(&r->logbuf->cond, &r->logbuf->lock);
+	pthread_mutex_unlock(&r->logbuf->lock);
+	pthread_mutex_unlock(&g_mutex);
+
+	return true;
 }
